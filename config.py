7687edf1413f780f397bb1b455ae2269f68e6c51
vkinash <kinash.job@gmail.com>
2023-11-18 17:17:25 +0100

    Added classes with weather API, added scheduler

--- config.py
+++ config.py
@@ -5,8 +5,11 @@ from functools import lru_cache
 class Settings(BaseSettings):
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
-    db_url: str = "postgresql://admin:admin@localhost:5432/db_url_shortener"
-    city: str = "Kiyv"
+    db_url: str = "sqlite:///./test.db?check_same_thread=False"
+    city: str = "Kyiv"
+    api_key: str = "17fe61111a3d5c289dfa7505eb46a034"
+    latitude: float = .0
+    longitude: float = .0
 
     class Config:
         env_file = ".env"
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi_utils.session import FastAPISessionMaker
+from fastapi_utils.tasks import repeat_every
+from config import get_settings
+from scheduler_app.tasks import task_add_current_temperature_in_city
 
 app = FastAPI()
 
@@ -6,6 +10,14 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Weather project v 1.0"}
+
+
+@app.on_event("startup")
+@repeat_every(seconds=3)  # 1 hour
+def task_scheduler() -> None:
+    task_add_current_temperature_in_city(city_name=get_settings().city,
+                                         lat=get_settings().latitude,
+                                         lon=get_settings().longitude)
 
 
 if __name__ == '__main__':
